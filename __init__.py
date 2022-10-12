5ce2062da430f3171d9eb6f37a445d8a0213c7c8
Royer.MR <royer.mendez@ucr.ac.cr>
2022-10-12 15:33:51 -0600

    Prueba de Royer

---
+++ __init__.py
@@ -0,0 +1,11 @@
+'''
+Creado el 12 de octubre del 2022
+
+@autor Royer Méndez Ramírez
+'''
+#%%
+# Hola mundo en Python
+#-------------------------------------------------------
+
+print("Hola Mundo ahora en Python3 !")
+#%%
